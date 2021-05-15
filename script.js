32cebfa2978d92bde3333627f0e4a42efd40f8d2
Adam Tucker <adamleetucker@outlook.com>
2021-05-15 20:43:41 +0000

    final interest change to script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100
-    var amount = parseFloat(principal)+parseFloat(interest)
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit "+"\<span class='highlightme'>"+principal+"\</span>"+",\<br\>at an interest rate of "+"\<span class='highlightme'>"+rate+"%"+"\</span>"+"\<br\>You will receive an amount of "+"\<span class='highlightme'>"+interest+"\</span>"+",\<br\>in the year "+"\<span class='highlightme'>"+year+"\</span>"+"\<br\>"
 
